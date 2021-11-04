4cb0eccc7d0d85524f03f96cb58528dd5b661441
Coldani3 <p110117069@gmail.com>
2021-11-04 12:17:18 +0000

    Add dynamic colours and stuff.

--- src/components/Bookshelf/Bookshelf.js
+++ src/components/Bookshelf/Bookshelf.js
@@ -6,7 +6,23 @@ function Bookshelf() {
 	const [items, setItems] = useState([
 		{
 			title: "Book of Doom", 
-			author: "Dark Lord Omega Supreme God Lazor X-Beam"
+			author: "Dark Lord Omega Supreme God Lazor X-Beam",
+			type: "Book"
+		},
+		{
+			title: "The Hitchhiker's Guide to the Galaxy",
+			author: "Douglas Adams",
+			type: "Book"
+		},
+		{
+			title: "Back to the Future",
+			author: "Uuuuuh the back to the future people?",
+			type: "Movie"
+		},
+		{
+			title: "Ayuh uh yeah *nonsensical gibberish about getting female dogs*: The Track: The Experience",
+			author: "Lil 5 Day Old Half Eaten Cheese Burger",
+			type: "CD"
 		}
 	]);
 
@@ -14,7 +30,7 @@ function Bookshelf() {
 		<div className="outer-border">
 			{
 				items.map((value, i) => 
-					<BookshelfItem key={i} title={value.title} author={value.author}/>
+					<BookshelfItem key={i} title={value.title} author={value.author} type={value.type}/>
 				)
 			}
 		</div>
--- src/components/BookshelfItem/BookshelfItem.js
+++ src/components/BookshelfItem/BookshelfItem.js
@@ -2,18 +2,26 @@ import React from 'react';
 
 import './BookshelfItem.css';
 
-function BookshelfItem({title, author, ...props}) {
+const ColorsForType = {
+	"Book": "blue",
+	"Movie": "red",
+	"CD": "pink"
+}
+
+function BookshelfItem({title, author, type, ...props}) {
 	return (
-		<div className="item-box">
-			<div className="title">{title}</div>
-			<div className="author">{author}</div>
+		<div className={"item-box " + ColorsForType[type]}>
+			<div className="title"><b>Title</b>: {title}</div>
+			<div className="author"><b>{type !== "Movie" ? "Author" : "Director"}</b>: {author}</div>
+			<div className="type"><b>Type</b>: {type}</div>
 		</div>
 	);
 }
 
 BookshelfItem.defaultProps = {
 	title: "Title",
-	author: "Author"
+	author: "Author",
+	type: "Book"
 }
 
 export default BookshelfItem;
